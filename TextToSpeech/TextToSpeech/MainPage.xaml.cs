573d9eacc472f143c700456b4434c049af617c16
User <User@DESKTOP-N6P2L92>
2022-12-30 23:23:59 +0800

    Add project files.

---
+++ TextToSpeech/TextToSpeech/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin.Forms.Xaml;
+
+namespace TextToSpeech
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonClicked(object sender, EventArgs e) 
+        {
+            await Xamarin.Essentials.TextToSpeech.SpeakAsync(EntryText.Text, new SpeechOptions
+            {
+                Volume = (float)SliderVolume.Value
+            });
+        }
+
+    }
+}
